6272e2901c66399b7332740ec925aee887728169
Zhijie Xia <zhijiexiacs@gmail.com>
2022-09-02 20:13:44 -0600

    Bug: programs never halt

---
+++ TCP_SCANNERS_PROXIES/tcp-scanner-slow/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// net.Dial(network, address string)
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+
+		conn, err := net.Dial("tcp", address)
+
+		if err != nil {
+			continue // fail to connect
+		}
+
+		// connected
+		conn.Close()
+		fmt.Printf("%d open\n", i)
+	}
+}
